9dc349d7c93c981df7f2af835441f0a2caa457b7
Sam <bsantos.sam08@gmail.com>
2025-03-03 15:55:57 -0500

    made spacing more consistent

--- src/main/cpp/subsystems/IntakeSubsystem.cpp
+++ src/main/cpp/subsystems/IntakeSubsystem.cpp
@@ -5,7 +5,11 @@
 IntakeSubsystem::IntakeSubsystem() : 
  motor1{},
  motor2{}
- 
+ {
+
+    
+ }
+
  void IntakeSubsystem::Forward(double speed) {
  double m1speed = speed * value1;
  double m2speed = speed * value2;
@@ -24,14 +28,14 @@ IntakeSubsystem::IntakeSubsystem() :
  if Get_X_Pressed {
   motor1.Set(speed);
  } else {
-    motor1.Set(0.0);
+  motor1.Set(0.0);
  }
  }
 
  void IntakeSubsystem::Drop() {
  if Get_Y_Pressed {
-    motor1.Set(speed)
+  motor1.Set(speed)
  } else {
-    motor1.Set(0.0)
+  motor1.Set(0.0)
  }
  }
